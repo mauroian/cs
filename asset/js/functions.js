1ad8c6553cceaa357aca3dfbec0f9b51a800404f
Fantantonio <antoniopanfili@gmail.com>
2023-03-17 23:03:31 +0100

    Sidebar + admin pages + navbar edits

---
+++ asset/js/functions.js
@@ -0,0 +1,27 @@
+
+const showHamburgerBorder = function () {
+    const hamburger = document.getElementById("cs-hamburger");
+    if (hamburger.classList.contains("cs-hamburger-open")) {
+        hamburger.classList.remove("cs-hamburger-open");
+    } else {
+        hamburger.classList.add("cs-hamburger-open");
+    }
+}
+
+const showSidebar = function () {
+    const sidebar = document.getElementById("cs-left-sidebar");
+    if (sidebar.classList.contains("cs-show")) {
+        sidebar.classList.remove("cs-show");
+    } else {
+        sidebar.classList.add("cs-show");
+    }
+}
+
+const showHide = function (elementId) {
+    const element = document.getElementById(elementId);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
